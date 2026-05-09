67e49ab358581311541ce257da1887cfd2f2cf95
Keerthi R <rkeerthiraj345@gmail.com>
2026-05-09 20:58:38 +0530

    Ficed resume issues

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_dj8hbq4ch,
-    api_key: process.env.CLOUDINARY_364336461475812,
-    api_secret: process.env.CLOUDINARY_NZ3_Tn4xX_oqy5TlwoBO_xOnf80,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
